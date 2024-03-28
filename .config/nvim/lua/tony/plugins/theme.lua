7c092c2ac114899172e440946a3797b477f8af25
Tony Adriansen <tony.adriansen@gmail.com>
2024-03-27 21:24:07 -0500

    nvim

--- .config/nvim/lua/tony/plugins/theme.lua
+++ .config/nvim/lua/tony/plugins/theme.lua
@@ -3,17 +3,66 @@ return { -- You can easily change to a different colorscheme.
   -- -- change the command in the config to whatever the name of that colorscheme is.
   -- --
   -- -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  -- 'folke/tokyonight.nvim',
+  'folke/tokyonight.nvim',
+  config = function()
+    require('tokyonight').setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      style = 'night', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = 'dark', -- style for sidebars, see below
+        floats = 'dark', -- style for floating windows
+      },
+      hide_inactive_statusline = true,
+      on_highlights = function(hl, c)
+        local prompt = '#2d3149'
+        hl.TelescopeNormal = {
+          bg = c.black,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    }
+  end,
 
-  'rose-pine/neovim',
+  -- 'rose-pine/neovim',
   -- 'bluz71/vim-moonfly-colors',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    -- vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'tokyonight-night'
     -- vim.cmd.colorscheme 'moonfly'
-    vim.cmd.colorscheme 'rose-pine'
+    -- vim.cmd.colorscheme 'rose-pine'
   end,
 }
