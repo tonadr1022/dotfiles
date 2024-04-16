baa7172dfbdb8ce38a2bbd4e1274bfbac85cf884
Tony Adriansen <tony.adriansen@gmail.com>
2024-04-16 08:14:20 -0500

    df

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,26 +1,31 @@
 return {
-  "echasnovski/mini.pairs",
-  event = "VeryLazy",
-  opts = {
-    mappings = {
-      -- ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-      -- ["``"] = { action = "closeopen", pair = "````", neigh_pattern = "[^\\``].", register = { cr = false } },
+  {
+    "echasnovski/mini.pairs",
+    opts = {
+      mappings = {
+        -- ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+        -- ["``"] = { action = "closeopen", pair = "````", neigh_pattern = "[^\\``].", register = { cr = false } },
 
-      ["`"] = false,
+        ["`"] = false,
+      },
+    },
+    keys = {
+      {
+        "<leader>up",
+        function()
+          vim.g.minipairs_disable = not vim.g.minipairs_disable
+          if vim.g.minipairs_disable then
+            LazyVim.warn("Disabled auto pairs", { title = "Option" })
+          else
+            LazyVim.info("Enabled auto pairs", { title = "Option" })
+          end
+        end,
+        desc = "Toggle Auto Pairs",
+      },
     },
   },
-  keys = {
-    {
-      "<leader>up",
-      function()
-        vim.g.minipairs_disable = not vim.g.minipairs_disable
-        if vim.g.minipairs_disable then
-          LazyVim.warn("Disabled auto pairs", { title = "Option" })
-        else
-          LazyVim.info("Enabled auto pairs", { title = "Option" })
-        end
-      end,
-      desc = "Toggle Auto Pairs",
-    },
+  {
+    "echasnovski/mini.comment",
+    enabled = false,
   },
 }
