7c4d2449026812b6c6397f85d82fbc6a94af32a0
Tony Adriansen <tony@tmachine.lan>
2025-01-29 07:28:10 -0600

    fix tmux sessionizer

--- bin/.local/scripts/tmux-sessionizer.sh
+++ bin/.local/scripts/tmux-sessionizer.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+	selected=$1
 else
-    selected=$(find ~/personal ~/class ~/ ~/personal/cpp \
-        ~/class/cs537 ~/dotfiles/nvim/.config -mindepth 1 -maxdepth 1 -type d | fzf)
+	selected=$(find ~/personal ~/class ~/ ~/personal/cpp \
+		-mindepth 1 -maxdepth 1 -type d 2>/dev/null | fzf)
 fi
 if [[ -z $selected ]]; then
-    exit 0
+	exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
+	tmux new-session -s $selected_name -c $selected
+	exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-    tmux new-session -ds $selected_name -c $selected
+	tmux new-session -ds $selected_name -c $selected
 fi
 
 if [[ -z $TMUX ]]; then
-    tmux attach -t $selected_name
+	tmux attach -t $selected_name
 else
-    tmux switch-client -t $selected_name
+	tmux switch-client -t $selected_name
 fi
