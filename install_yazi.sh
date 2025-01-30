1673b71165f617094b8ba550b8640c4c59de1a21
Tony Adriansen <tony.adriansen@gmail.com>
2025-01-30 06:32:12 -0600

    yazi

---
+++ install_yazi.sh
@@ -0,0 +1,4 @@
+sudo apt install ffmpeg 7zip jq poppler-utils fd-find ripgrep xclip
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+rustup update
+cargo install --locked yazi-fm yazi-cli
