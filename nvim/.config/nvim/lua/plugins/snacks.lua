e2fcb70115b11f2745a796c1db86dea1e3657fdb
Tony Adriansen <tony.adriansen@gmail.com>
2026-08-01 21:18:22 -0500

    snacks llua

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -20,6 +20,18 @@ return {
  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝╚═╝         ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
                                                                                          
 ]],
+          -- stylua: ignore
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua require('fff').find_files()" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua require('fff').live_grep()" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
         },
       },
       scroll = { enabled = false },
